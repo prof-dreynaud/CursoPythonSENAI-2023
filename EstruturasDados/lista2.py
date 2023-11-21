f3544f73677151e4fa2e2ba3dd886f4f55f2e5a5
Prof Diego <diego.reynaud@edu.sc.senai.br>
2023-11-21 20:13:42 -0300

    lista 2 ok

---
+++ EstruturasDados/lista2.py
@@ -0,0 +1,16 @@
+numeros = [5,2,8,1,9]
+print(numeros)
+
+#ordenar os nros
+numeros.sort()
+print(numeros)
+
+#soma
+soma = sum(numeros)
+print(soma)
+
+#máximo e mínimo
+minimo = min(numeros)
+maximo = max(numeros)
+
+print(minimo,"\n",maximo)
