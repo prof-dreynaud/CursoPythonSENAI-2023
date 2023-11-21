c46dbea0f220029b4626984a0e83340640513df4
Prof Diego <diego.reynaud@edu.sc.senai.br>
2023-11-21 20:07:08 -0300

    update lista 1

--- EstruturasDados/lista1.py
+++ EstruturasDados/lista1.py
@@ -4,3 +4,16 @@ nomes = ["Luan","Gustavo","Ester", "Iago"]
 #Percorrer a lista
 for nome in nomes:
     print(nome)
+
+#Procurar um nome na lista
+if "Vinicius" in nomes:
+    print("Aluno está presente!")
+
+else:
+    print("Aluno faltou!")
+
+#Concatenar outra lista
+outros_nomes = ["Helder", "Aduyar"]
+todos_alunos = nomes + outros_nomes
+
+print(todos_alunos)
